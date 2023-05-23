165e75e51d16c4133f2617b4bc3d11e3200e17f8
michael <oy.xt@qq.com>
2023-05-23 15:45:39 +0800

    doc: 给router-link、router-view添加注释

--- src/components/link.js
+++ src/components/link.js
@@ -18,33 +18,44 @@ let warnedEventProp
 export default {
   name: 'RouterLink',
   props: {
+    // 目标路由
     to: {
       type: toTypes,
       required: true
     },
+    // 渲染的标签元素
     tag: {
       type: String,
       default: 'a'
     },
     custom: Boolean,
+    // 精准匹配
     exact: Boolean,
     exactPath: Boolean,
     append: Boolean,
     replace: Boolean,
+    // 定义激活时的类名
     activeClass: String,
+    // 定义精准匹配后激活的类名
     exactActiveClass: String,
     ariaCurrentValue: {
       type: String,
       default: 'page'
     },
+    // 添加的dom事件
     event: {
       type: eventTypes,
       default: 'click'
     }
   },
-  render (h: Function) {
+  // h为this.$createElement，会注入到组件内
+  render(h: Function) {
+    // VueRouter实例
     const router = this.$router
+    // 当前路由
     const current = this.$route
+    // 解析 to 的路径对应路由项
+    // router.resolve方法核心是调用match方法
     const { location, route, href } = router.resolve(
       this.to,
       current,
@@ -52,44 +63,62 @@ export default {
     )
 
     const classes = {}
+    // 用户自定义的点击激活后的类
     const globalActiveClass = router.options.linkActiveClass
+    // 用户自定义的点击精准匹配 激活后的类
     const globalExactActiveClass = router.options.linkExactActiveClass
     // Support global empty active class
+    // 激活类降级 用户没有定义就使用默认值
     const activeClassFallback =
       globalActiveClass == null ? 'router-link-active' : globalActiveClass
+    // 精准匹配激活类降价 用户没有定义就使用默认值
     const exactActiveClassFallback =
       globalExactActiveClass == null
         ? 'router-link-exact-active'
         : globalExactActiveClass
+    // 在父组件内也可以定义
+    // 权重优先级 组件内 > 全局设置
     const activeClass =
       this.activeClass == null ? activeClassFallback : this.activeClass
     const exactActiveClass =
       this.exactActiveClass == null
         ? exactActiveClassFallback
         : this.exactActiveClass
-
+    
+    // 根据当前路由设置当前对象
+    // 跳转到目的地路由
     const compareTarget = route.redirectedFrom
       ? createRoute(null, normalizeLocation(route.redirectedFrom), null, router)
       : route
 
+    // 如果严格模式的话 就判断是否是相同路由（path query params hash）
+    // 否则就走包含逻辑（path包含，query包含 hash为空或者相同）
     classes[exactActiveClass] = isSameRoute(current, compareTarget, this.exactPath)
     classes[activeClass] = this.exact || this.exactPath
       ? classes[exactActiveClass]
       : isIncludedRoute(current, compareTarget)
 
     const ariaCurrentValue = classes[exactActiveClass] ? this.ariaCurrentValue : null
-
+    
+    // 事件处理函数
     const handler = e => {
       if (guardEvent(e)) {
+        // 路由replace触发改变router-view
         if (this.replace) {
           router.replace(location, noop)
-        } else {
+        }
+        // 路由push触发改变router-view
+        else {
           router.push(location, noop)
         }
       }
     }
 
+    // 事件对象
+    // guardEvent 阻止一些组合操作
     const on = { click: guardEvent }
+    // this.event为外界传入的 默认为click
+    // 所以上方的guardEvent会被覆盖
     if (Array.isArray(this.event)) {
       this.event.forEach(e => {
         on[e] = handler
@@ -98,8 +127,12 @@ export default {
       on[this.event] = handler
     }
 
+    // 添加元素的类
     const data: any = { class: classes }
 
+    // 作用域插槽，只支持name为default的插槽
+    // 作用域插槽的组件用函数包裹了，因此可以传参props
+    // 这里的props是子组件内部的数据
     const scopedSlot =
       !this.$scopedSlots.$hasNormal &&
       this.$scopedSlots.default &&
@@ -111,6 +144,9 @@ export default {
         isExactActive: classes[exactActiveClass]
       })
 
+    // 作用域插槽只能获取href, route, navigate, isActive, isExactActive这五个值
+    // 默认返回<span>标签包括的元素
+    // https://router.vuejs.org/zh/api/#router-link
     if (scopedSlot) {
       if (process.env.NODE_ENV !== 'production' && !this.custom) {
         !warnedCustomSlot && warn(false, 'In Vue Router 4, the v-slot API will by default wrap its content with an <a> element. Use the custom prop to remove this warning:\n<router-link v-slot="{ navigate, href }" custom></router-link>\n')
@@ -130,7 +166,7 @@ export default {
         return scopedSlot.length === 0 ? h() : h('span', {}, scopedSlot)
       }
     }
-
+    
     if (process.env.NODE_ENV !== 'production') {
       if ('tag' in this.$options.propsData && !warnedTagProp) {
         warn(
@@ -148,18 +184,25 @@ export default {
       }
     }
 
+    // 没有作用域插槽就根据tag标签做处理，tag默认为a
     if (this.tag === 'a') {
+      // 添加回调函数，这里创建的是真实元素
+      // 在vue中data.on给了$listeners, data.nativeOn的值给了data.on
       data.on = on
+      // 添加href属性
       data.attrs = { href, 'aria-current': ariaCurrentValue }
     } else {
       // find the first <a> child and apply listener and href
+      // 如果没有a标签就寻找 普通默认插槽的第一个子标签为a的标签
       const a = findAnchor(this.$slots.default)
       if (a) {
         // in case the <a> is a static node
+        // 这里主要是对a标签，做新增事件处理
         a.isStatic = false
         const aData = (a.data = extend({}, a.data))
         aData.on = aData.on || {}
         // transform existing events in both objects into arrays so we can push later
+        // 处理a标签的事件（用数组保存），便于后续处理
         for (const event in aData.on) {
           const handler = aData.on[event]
           if (event in on) {
@@ -167,6 +210,7 @@ export default {
           }
         }
         // append new listeners for router-link
+        // 添加新的事件
         for (const event in on) {
           if (event in aData.on) {
             // on[event] is always a function
@@ -181,33 +225,40 @@ export default {
         aAttrs['aria-current'] = ariaCurrentValue
       } else {
         // doesn't have <a> child, apply listener to self
+        // 找不到子标签a，就给data添加on回调，创建对应的元素
         data.on = on
       }
     }
-
+    // 渲染router-link
     return h(this.tag, data, this.$slots.default)
   }
 }
 
 function guardEvent (e) {
   // don't redirect with control keys
+  // 忽略带有功能键的点击
   if (e.metaKey || e.altKey || e.ctrlKey || e.shiftKey) return
   // don't redirect when preventDefault called
+  // 已阻止的返回
   if (e.defaultPrevented) return
   // don't redirect on right click
+  // 右击
   if (e.button !== undefined && e.button !== 0) return
   // don't redirect if `target="_blank"`
+  // `target="_blank"` 忽略
   if (e.currentTarget && e.currentTarget.getAttribute) {
     const target = e.currentTarget.getAttribute('target')
     if (/\b_blank\b/i.test(target)) return
   }
   // this may be a Weex event which doesn't have this method
+  // 阻止默认行为 防止跳转
   if (e.preventDefault) {
     e.preventDefault()
   }
   return true
 }
 
+// 找到第一个A标签
 function findAnchor (children) {
   if (children) {
     let child
--- src/components/view.js
+++ src/components/view.js
@@ -17,30 +17,45 @@ export default {
 
     // directly use parent context's createElement() function
     // so that components rendered by router-view can resolve named slots
+    // 直接使用父组件上下文的createElement()函数
     const h = parent.$createElement
+    // 组件上的name属性
     const name = props.name
+    // 父组件的路由，即history.current
+    // 取值，做了一次依赖手机，加入响应式系统
     const route = parent.$route
+    // 做一层缓存
     const cache = parent._routerViewCache || (parent._routerViewCache = {})
 
     // determine current view depth, also check to see if the tree
     // has been toggled inactive but kept-alive.
+    // 这个深度是为了解决路由嵌套问题而存在的，因此matched数组中record的排序是父到子
     let depth = 0
     let inactive = false
+    // 从当前组件一直遍历到最外层的根组件app.vue
+    // 解决router-view 嵌套问题
     while (parent && parent._routerRoot !== parent) {
+      // 获取vnode的data
       const vnodeData = parent.$vnode ? parent.$vnode.data : {}
+      // routerView属性标记当前vnode组件是否为一个路由组件
       if (vnodeData.routerView) {
         depth++
       }
+      // 在keep-alive中非激活状态
       if (vnodeData.keepAlive && parent._directInactive && parent._inactive) {
         inactive = true
       }
+      // 由内而外遍历
       parent = parent.$parent
     }
     data.routerViewDepth = depth
 
     // render previous view if the tree is inactive and kept-alive
+    // 组件被keep-alive缓存了
     if (inactive) {
+      // 读取缓存数据
       const cachedData = cache[name]
+      // 读取缓存组件
       const cachedComponent = cachedData && cachedData.component
       if (cachedComponent) {
         // #2301
@@ -48,27 +63,35 @@ export default {
         if (cachedData.configProps) {
           fillPropsinData(cachedComponent, data, cachedData.route, cachedData.configProps)
         }
+        // 渲染缓存的组件
         return h(cachedComponent, data, children)
       } else {
         // render previous empty view
+        // 没有缓存的组件就渲染空组件
         return h()
       }
     }
 
+    // matched是一个数组，获取matched的深度，也就是层级关系
+    // matched本质上就是record对象
     const matched = route.matched[depth]
+    // 因为只有record对象才有component
+    // 对于没有name属性的routerView而言，name为default
     const component = matched && matched.components[name]
 
     // render empty node if no matched route or no config component
+    // 找不到record或者没有对应的组件就清除缓存，并渲染空数组
     if (!matched || !component) {
       cache[name] = null
       return h()
     }
 
-    // cache component
+    // 缓存组件
     cache[name] = { component }
 
     // attach instance registration hook
     // this will be called in the instance's injected lifecycle hooks
+    // beforeCreated生产周期内被调用，将组件实例保存在record对象中
     data.registerRouteInstance = (vm, val) => {
       // val could be undefined for unregistration
       const current = matched.instances[name]
@@ -82,12 +105,15 @@ export default {
 
     // also register instance in prepatch hook
     // in case the same component instance is reused across different routes
+    // prepatch hookd中注册回调函数，更新组件数据
     ;(data.hook || (data.hook = {})).prepatch = (_, vnode) => {
       matched.instances[name] = vnode.componentInstance
     }
 
     // register instance in init hook
     // in case kept-alive component be actived when routes changed
+    // init hook 注册回调函数，init扣子在keep-alive激活的时候被调用
+    // 更新数据
     data.hook.init = (vnode) => {
       if (vnode.data.keepAlive &&
         vnode.componentInstance &&
@@ -102,6 +128,7 @@ export default {
       handleRouteEntered(route)
     }
 
+    // record
     const configProps = matched.props && matched.props[name]
     // save route and configProps in cache
     if (configProps) {
@@ -112,6 +139,7 @@ export default {
       fillPropsinData(component, data, route, configProps)
     }
 
+    // 渲染组件
     return h(component, data, children)
   }
 }
